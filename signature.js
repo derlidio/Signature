35c573ff10579e8edef9b5b001b7ff149a457be8
Derlidio Siqueira <derlidio@users.noreply.github.com>
2022-10-23 20:31:40 -0300

    Update to also work with mobile touch events.

--- signature.js
+++ signature.js
@@ -9,23 +9,45 @@ function add_events()
     pen = document.getElementById("pen");
     clear_button = document.getElementById("clear");
 
-    signature.onmousemove = function(event){ trace(event); }
+    signature.onmousemove = function(event){ trace(event, true); }
     signature.onmouseleave = function() { pressed = false; }
+
+    signature.ontouchmove = function(event){ trace(event, false); }
+    signature.ontouchend = function() { pressed = false; }
+
     clear_button.onclick= function(){ clear(); }
 }
 
-function trace(event)
+function trace(event, mouse)
 {
-    if (event.buttons != 1) { pressed = false; return; }
+    var px;
+    var py;
+
+    if (mouse)
+    {
+        if (event.buttons != 1) 
+        { 
+            pressed = false; return; 
+        }
+        px = event.pageX;
+        py = event.pageY;
+    }
+    else
+    {
+        px = event.touches[0].clientX;
+        py = event.touches[0].clientY;
+    }
 
     const rect = signature.getBoundingClientRect();
 
-    var x = event.x - rect.x;
-    var y = event.y - rect.y;
+    var x = px - rect.x;
+    var y = py - rect.y;
 
     data += (pressed ? "L" : "M") + x + "," + y;
 
     pen.setAttribute("d", data);
+
+    event.preventDefault();
 
     pressed = true;
 }
